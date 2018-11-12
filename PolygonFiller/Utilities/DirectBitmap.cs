7bb6ffa4195fff4da168b4cff1b379208c47d81a
Sebastian Safin <sebastian.safin@outlook.com>
2018-11-12 20:08:07 +0100

    Added FastBitmap class. Moved DirectBitmap class to separate file.

---
+++ PolygonFiller/Utilities/DirectBitmap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace PolygonFiller
+{
+    public class DirectBitmap : IDisposable
+    {
+        public Bitmap Bitmap { get; private set; }
+        public Int32[] Bits { get; private set; }
+        public bool Disposed { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+        public Point BitmapLocationForDrawing { get; set; }
+
+        protected GCHandle BitsHandle { get; private set; }
+
+        public DirectBitmap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Bits = new Int32[width * height];
+            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
+            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
+        }
+
+        public void SetPixel(int x, int y, int color)
+        {
+            int index = x + (y * Width);
+            Bits[index] = color;
+        }
+
+        public void SetPixel(int x, int y, Color color)
+        {
+            SetPixel(x, y, color.ToArgb());
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            int index = x + (y * Width);
+            int col = Bits[index];
+            Color result = Color.FromArgb(col);
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (Disposed)
+            {
+                return;
+            }
+
+            Disposed = true;
+            Bitmap.Dispose();
+            BitsHandle.Free();
+        }
+    }
+}
---
+++ PolygonFiller/Utilities/FastBitmap.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PolygonFiller
+{
+    public unsafe class FastBitmap : IDisposable
+    {
+        private readonly Bitmap bitmap;
+        private BitmapData bitmapData;
+        private byte* scan0;
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int BytesPerPixel { get; private set; }
+        public int Depth { get; private set; }
+        public bool Locked { get; private set; }
+
+        public FastBitmap(Bitmap bitmap)
+        {
+            this.bitmap = bitmap;
+
+            Width = bitmap.Width;
+            Height = bitmap.Height;
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            byte* currentLine = scan0 + (y * bitmapData.Stride);
+            x *= BytesPerPixel;
+
+            switch (Depth)
+            {
+                case 32:
+                    return Color.FromArgb(currentLine[x + 3], currentLine[x + 2], currentLine[x + 1], currentLine[x]);
+                case 24:
+                    return Color.FromArgb(currentLine[x + 2], currentLine[x + 1], currentLine[x]);
+                default:
+                    return Color.FromArgb(currentLine[x], currentLine[x], currentLine[x]);
+            }
+        }
+
+        public void Lock()
+        {
+            if (Locked)
+            {
+                throw new InvalidOperationException("Bitmap is already locked");
+            }
+
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            bitmapData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            scan0 = (byte*)bitmapData.Scan0;
+
+            Depth = Image.GetPixelFormatSize(bitmap.PixelFormat);
+            BytesPerPixel = Depth / 8;
+            Locked = true;
+        }
+
+        public void Unlock()
+        {
+            if (!Locked)
+            {
+                throw new InvalidOperationException("Bitmap is not locked");
+            }
+
+            bitmap.UnlockBits(bitmapData);
+            Locked = false;
+        }
+
+        public void Dispose()
+        {
+            if (Locked)
+            {
+                Unlock();
+            }
+        }
+    }
+
+    public static class FastBitmapExtensions
+    {
+        public static FastBitmap GetLockedFastBitmap(this Bitmap bitmap)
+        {
+            FastBitmap fastBitmap = new FastBitmap(bitmap);
+            fastBitmap.Lock();
+
+            return fastBitmap;
+        }
+    }
+}
